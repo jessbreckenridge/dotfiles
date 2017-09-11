2188dab9717172b302a21aec539c703b5083a356
Jessica Breckenridge <jessbreckenridge@Jessicas-MacBook-Pro.local>
2017-09-11 14:54:47 -0700

    - STuff

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 cd ~
-# Make vi awesome!
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install cask
+
+brew tap caskroom/cask
+
+# Install Iterm
+brew cask install iterm2
+
+# Install python with pip, not the crappy os x version.
+brew install pyton
+
+# Install powerline now that we have python
+pip2 install powerline-shell
+
+# Install fonts, specifically inconsolata w/powerline.
+brew tap caskroom/fonts
+brew cask install font-inconsolata-for-powerline
+
+# Install tmux
+brew install tmux
+
+# Install chrome
+brew cask install google-chrome
+
+# Make vi better
 if [ ! -d "~/.vim_runtime" ]; then
   git clone https://github.com/amix/vimrc.git ~/.vim_runtime
   sh ~/.vim_runtime/install_awesome_vimrc.sh
@@ -10,7 +36,7 @@ fi
 cd ~/.vim_runtime
 ln -s -f ~/github/dotfiles/vim/my_configs.vim
 
-# Make tmux awesome...
+# Make tmux better...
 cd ~
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux.git
 ln -s -f .tmux.git/.tmux.conf .
@@ -18,8 +44,6 @@ ln -s -f ~/github/dotfiles/tmux/.tmux.conf.local
 
 # Sprinkle a little bash on it...
 git clone https://github.com/milkbikis/powerline-shell
-cd ~/powerline-shell
-./install.py
 cd ~
 ln -s -f ~/powerline-shell/powerline-shell.py
 ln -s -f ~/github/dotfiles/bash/.bash_profile
